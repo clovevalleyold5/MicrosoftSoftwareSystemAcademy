37545cd17c362125525cfc478ef9e101115030e6
Five <kaiyuan.wang@live.cn>
2020-04-16 12:17:59 -0400

    remake

---
+++ Quiz/20200415/Five/Controllers/HomerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Five.Models;
+using System.Linq;
+
+namespace Five.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ActionResult Index() => View(SimpleRepository.SharedRepository.People);
+        public ActionResult Index(string searchby, string search)
+        {
+            if (searchby == "Gender")
+            {
+                return View(SimpleRepository.SharedRepository.People.Where(p => p.Gender == search || search == null));
+            }
+            else if (searchby == "Name")
+            {
+                return View(SimpleRepository.SharedRepository.People.Where(p => p.Name==search || search == null));
+            }
+            else if (searchby == "Height")
+            {
+                return View(SimpleRepository.SharedRepository.People.Where(p => p.Height == search || search == null));
+            }
+            else
+            {
+                return View(SimpleRepository.SharedRepository.People.Where(p => p.Weight == search || search == null));
+            }
+        }
+    }
+}
---
+++ Quiz/20200415/Five/Models/SimpleRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Five.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository sharedrepository = new SimpleRepository();
+        private Dictionary<string, Person> people = new Dictionary<string, Person>();
+        public static SimpleRepository SharedRepository => sharedrepository;
+        public SimpleRepository()
+        {
+            var group = new[]
+            {
+                new Person{Name="Jameson Locke",Height="208",Weight="116",Gender="Male"},
+                new Person{Name="Holly Tanaka",Height="201",Weight="108",Gender="Female"},
+                new Person{Name="Olympia Vale",Height="206",Weight="102",Gender="Female"},
+                new Person{Name="Edward Buck",Height="206",Weight="113",Gender="Male"}
+            };
+            foreach (var p in group)
+            {
+                AddPerson(p);
+            }
+        }
+        public IEnumerable<Person> People => people.Values;
+        public void AddPerson(Person p) => people.Add(p.Name, p);
+    }
+}
